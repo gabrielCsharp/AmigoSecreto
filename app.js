7190d595b04e4d876b49f2f7b67ec98326244b88
23Edu4rd0 <23eduardoviana@gmail.com>
2025-01-18 20:09:39 -0300

    Amo o cliff

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+
+
+
+if (nome == "") {
+    alert("Por Favor, Insira um nome")
+} else {
+    console.log("Odeio JavaScript")
+}
+
+function limparCampo() {
+    chute = document.querySelector("input");
+    chute.value = "";
+}
+// odeio o cliff S2
