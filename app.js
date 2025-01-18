614eca3a1d9d22f180f1f80d718e36f132bc7ae9
KernelOS <gabrielhenriquenovo84@gmail.com>
2025-01-18 20:12:11 -0300

    perdao

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nome = document.querySelector('#amigo');
+let listaAmigos = [];
+
+
+function adicionarAmigo(){
+listaAmigos.push(nome.value);
+
+     //console.log(listaAmigos);
+     //console.log(nome.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ta jogando dentro da let listaAmigos
+// let listaAmigos = [function adicionarAmigo(){
+//listaAmigos.push(nome.value);];
+
+
